20f818cb3f14e7d0af122020b4e1cdbe021d6000
McDiod <hobbeldobbel@gmail.com>
2019-01-27 20:21:10 +0100

    update to tvt_template

--- cfgParams.hpp
+++ cfgParams.hpp
@@ -2,9 +2,9 @@ class Params
 {
     class PREPARATION_TIME {
         title = "Vorbereitungszeit";
-        values[] = {0,20,60,120,180,240,300,480,600,900};
-        texts[] = {"off","20s","1min","2min","3min","4min","5min","8min","10min","15min"};
-        default = 20;
+        values[] = {0,60,120,180,240,300,480,600,900};
+        texts[] = {"off","1min","2min","3min","4min","5min","8min","10min","15min"};
+        default = 0;
     };
 
     #include "USER\userParams.hpp"
